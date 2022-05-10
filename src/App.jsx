e289c1e2308cad1c2efd2aeab2f53fdc0e35286b
Henry Romero <hromerp22@gmail.com>
2022-05-10 12:57:02 -0400

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom"
+import Login from "./routes/Login"
+import Home from "./routes/Home"
+import Navbar from "./components/Navbar"
+import RequiereAuth from "./components/RequireAuth"
+const App = () => {
+  return (
+    <>
+  <Navbar />
+  <h1>APP</h1>
+    <Routes>
+        <Route path="/" element={<RequiereAuth><Home /></RequiereAuth>} />
+        <Route path="/login" element={<Login />} />
+   
+</Routes>
+    </>
+  )
+}
+
+export default App
